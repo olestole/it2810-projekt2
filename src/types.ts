98af9d2ac1f69af8169c4a6f05e9bffc5755a027
Sverre Rynning-Tønnesen <sverrert@MacBook-Pro.local>
2020-09-14 19:20:52 +0200

    #6, made sun svg and fixed layout resize

--- src/types.ts
+++ src/types.ts
@@ -24,5 +24,5 @@ export interface IPoem {
   lines: string[];
 }
 
-export type AnimationType = 'cloud' | 'stars' | 'heart';
+export type AnimationType = 'cloud' | 'stars' | 'heart' | 'sun';
 export type SVGUserType = 'man1' | 'man2' | 'man3' | 'man4' | 'man5' | 'woman1' | 'woman2' | 'woman3' | 'woman4';
