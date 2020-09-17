e3679df90ee0a1c2cfb1db94a4a1456b5c11e7c7
Sverre Rynning-Tønnesen <sverrert@MacBook-Pro.local>
2020-09-17 18:28:58 +0200

    #9 Working on making tests work

--- src/tests/Profile.test.tsx
+++ src/tests/Profile.test.tsx
@@ -3,11 +3,13 @@ import { render, act, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import App from 'components/App';
+import Header from '../components/Heading/Header';
 
 describe('Profile test-suite', () => {
   test('profile renders', () => {
-    render(<App />);
-
-    expect(screen.getByText('Halla bro')).toBeInTheDocument();
+    //render(<Header />);
+    const wrapper = <Header></Header>;
+    expect(wrapper).toMatchSnapshot();
+    //expect(screen.getByText('')).toBeInTheDocument();
   });
 });
