6985b8f6e16f3faa93783525c83dfe2b0314da2a
Sverre Rynning-Tønnesen <sverrert@dhcp-10-22-1-11.wlan.ntnu.no>
2020-09-18 15:38:53 +0200

    #10, added songs

--- src/pages/ProfileView.tsx
+++ src/pages/ProfileView.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import AppContext from 'utils/AppContext';
 import { Poem } from 'components/Profile';
+import song from 'utils/songsConverter';
 
 const ProfileView = () => {
   const { appState, appDispatch } = useContext(AppContext);
@@ -10,6 +11,10 @@ const ProfileView = () => {
       <h1>ProfileView</h1>
       <h2>{appState.currentUser?.name}</h2>
       <Poem />
+      <audio controls>
+        <source src={song(appState.currentUser != null ? appState.currentUser?.song : '')} type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
     </div>
   );
 };
