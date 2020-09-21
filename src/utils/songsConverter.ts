ba67383c147895b9e2cdac6ebb2f8b5120bda856
Sverre Rynning-Tønnesen <sverrert@MacBook-Pro.local>
2020-09-21 16:06:30 +0200

    test now working again

--- src/utils/songsConverter.ts
+++ src/utils/songsConverter.ts
@@ -1,23 +1,17 @@
-const song1 = require('assets/mp3/song1.mp3');
-const song2 = require('assets/mp3/song2.mp3');
-const song3 = require('assets/mp3/song3.mp3');
-const song4 = require('assets/mp3/song4.mp3');
-const song5 = require('assets/mp3/song5.mp3');
-
 //https://soundcloud.com/hanangobran/sets/reading-music
 
 const song = (songName: string) => {
   switch (songName) {
     case 'song1':
-      return song1;
+      return require('assets/mp3/song1.mp3');
     case 'song2':
-      return song2;
+      return require('assets/mp3/song2.mp3');
     case 'song3':
-      return song3;
+      return require('assets/mp3/song3.mp3');
     case 'song4':
-      return song4;
+      return require('assets/mp3/song4.mp3');
     case 'song5':
-      return song5;
+      return require('assets/mp3/song5.mp3');
     default:
       return null;
   }
