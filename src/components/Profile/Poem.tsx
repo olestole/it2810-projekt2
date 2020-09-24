33732972fafa52f306d93243c3298173d10f93e6
Ole August <oleastole@gmail.com>
2020-09-24 14:35:40 +0200

    #21: Fix how poems are loaded and rendered

--- src/components/Profile/Poem.tsx
+++ src/components/Profile/Poem.tsx
@@ -1,8 +1,8 @@
 import React, { useContext, useState, useEffect } from 'react';
 import AppContext from 'utils/AppContext';
-import Refresh from './refresh.png';
 import Spinner from 'components/Spinner';
 import { IPoem } from 'types';
+import { AiOutlineReload } from 'react-icons/ai';
 
 import { fetchPoem } from '../../utils/fetchPoem';
 
@@ -10,49 +10,62 @@ import './profile.css';
 
 const Poem = () => {
   const { appState, appDispatch } = useContext(AppContext);
-  const [currentPoem, setCurrentPoem] = useState<IPoem[] | undefined>();
-  let randNumber = 1;
+  const [currentPoemList, setCurrentPoemList] = useState<IPoem[] | undefined>();
+  const [currentPoem, setCurrentPoem] = useState<IPoem | undefined>();
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+  useEffect(() => {
+    getPoem();
+  }, []);
 
   const getPoem = async () => {
     if (appState.currentUser) {
       const poemResponse = await fetchPoem(appState.currentUser?.favAuthor);
       if (poemResponse != null) {
-        setCurrentPoem(poemResponse as IPoem[]);
+        setCurrentPoemList(poemResponse as IPoem[]);
+        genereateRandomPoem(poemResponse as IPoem[]);
       }
     }
   };
 
-  useEffect(() => {
-    if (appState.currentUser) {
-      getPoem();
+  const genereateRandomPoem = (poemlist?: IPoem[]) => {
+    // Needs to do this optional direct passing of poemlist as useState isn't synchronous --> Doesn't get a poem on first load
+    const poemList = currentPoemList ? currentPoemList : poemlist ? poemlist : null;
+    if (poemList) {
+      setCurrentPoem(poemList[currentIndex]);
+      setCurrentIndex((currentIndex + 1) % poemList.length);
     }
-  }, []);
+  };
+
+  const truncatePoemLines = (poem: IPoem, length: number = 30): string[] => {
+    if (poem.linecount > 30) {
+      return [...poem.lines.slice(0, length), '...'];
+    }
+    return poem.lines;
+  };
 
   const RenderLinesString = () => {
-    if (currentPoem) {
-      randNumber = Math.floor(Math.random() * currentPoem.length);
+    if (currentPoemList) {
       return (
         <div id="poem">
-          <h1>{currentPoem ? currentPoem[randNumber].title : null}</h1>
-          {currentPoem[randNumber].lines.map((line: string, index: number) => (
-            <p>{line}</p>
+          <h1>{currentPoem?.title}</h1>
+          {truncatePoemLines(currentPoem!).map((line: string, index: number) => (
+            <p key={index}>{line}</p>
           ))}
-          <h3>- {currentPoem ? currentPoem[randNumber].author : null}</h3>
+          <h3>- {currentPoem?.author}</h3>
         </div>
       );
     }
-    return (
-      <div id="poem">
-        <h3>Loading poem...</h3>
-        <Spinner />
-      </div>
-    );
   };
 
   return (
-    <div id="poemContainer">
-      {/* <img src={Refresh} onClick={getPoem}></img> */}
-      {RenderLinesString()}
+    <div className="mainContainer">
+      {currentPoem ? (
+        <div className="reloadContainer">
+          <AiOutlineReload id="reloadButton" onClick={() => genereateRandomPoem()} />
+        </div>
+      ) : null}
+      <div id="poemContainer">{currentPoem ? RenderLinesString() : <div id="spinner">{Spinner()}</div>}</div>
     </div>
   );
 };
