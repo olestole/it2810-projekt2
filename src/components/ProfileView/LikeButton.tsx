31a81a1941830183227660df1f8246351c86e981
Sverre Rynning-Tønnesen <sverrert@dhcp-10-22-2-102.wlan.ntnu.no>
2020-09-25 13:10:31 +0200

    resolved merge conflict

--- src/components/ProfileView/LikeButton.tsx
+++ src/components/ProfileView/LikeButton.tsx
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import './likeButton.css';
-=======
->>>>>>> 215f004c36727aabd43c0979fa02186380f5dd2a
 
 interface Likefunction {
   like: () => void;
@@ -14,14 +11,10 @@ export default class LikeButton extends Component<Likefunction> {
   }
 
   render() {
-<<<<<<< HEAD
     return (
       <button id="likeButton" onClick={this.props.like}>
         Like
       </button>
     );
-=======
-    return <button onClick={this.props.like}>Like</button>;
->>>>>>> 215f004c36727aabd43c0979fa02186380f5dd2a
   }
 }
