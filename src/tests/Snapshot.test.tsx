fde5539694e2a88a22b08c3b1ba6a812635ccb43
Sverre Rynning-Tønnesen <sverrert@MacBook-Pro.local>
2020-09-27 16:46:48 +0200

    #20, added function testing

--- src/tests/Snapshot.test.tsx
+++ src/tests/Snapshot.test.tsx
@@ -1,32 +1,18 @@
 import React from 'react';
-import { render, act, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import * as renderer from 'react-test-renderer';
-import Header from '../components/Heading/Header';
 import AppContextProvider from '../components/AppContextProvider';
 import App from '../components/App';
-//import { shallow, configure, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-//configure({ adapter: new Adapter() });
-
-describe('Header component', () => {
-  it('matches the snapshot', () => {
-    const tree = renderer.create(<Header />).toJSON();
-
-    expect(tree).toMatchSnapshot();
-  });
-});
 
 describe('Whole page', () => {
   it('matches the snapshot', () => {
-    const shallowWrapper = renderer.create(
+    const wholePage = renderer.create(
       <AppContextProvider>
         <App />
       </AppContextProvider>,
     );
 
-    expect(shallowWrapper).toMatchSnapshot();
+    expect(wholePage).toMatchSnapshot();
   });
 });
